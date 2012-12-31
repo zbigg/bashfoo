2cdec6c33807c05d0c072805e27f6bd1cd10cdf5
z.zagorski <z.zagorski@gmail.com>
2012-12-31 11:10:53 +0000

    bashfoo: fixed several bashfoo path evaulations

--- bashfoo.sh
+++ bashfoo.sh
@@ -4,13 +4,13 @@
 #   bootstrap bashfoo environment in script
 #  
 
-if [ -z "$bashfoo_source_path" ] ; then
-    echo "bashfoo_source_path not set"
+if [ -z "$bashfoo_libdir" ] ; then
+    echo "bashfoo_libdir not set"
     exit 1
 fi
 
-source "$bashfoo_source_path/log.sh"
-source "$bashfoo_source_path/flag.sh"
+source "$bashfoo_libdir/log.sh"
+source "$bashfoo_libdir/flag.sh"
 
 set_flag bashfoo_loaded_modules log
 set_flag bashfoo_loaded_modules flag
@@ -21,7 +21,7 @@ bashfoo_require()
     
     local module_loaded_flag="${module}_loaded" 
     if ! check_flag bashfoo_loaded_modules $module ; then
-        local module_path="$bashfoo_source_path/$module.sh"
+        local module_path="$bashfoo_libdir/$module.sh"
         log_debug "loading bashfoo module $module_path"
         source $module_path
         
