1947054a430c131433ef6a92e785b68101de2e62
z.zagorski <z.zagorski@gmail.com>
2013-12-13 21:45:04 +0000

    run: remove spurious diagnostics (-x, -v)

--- run.sh
+++ run.sh
@@ -23,13 +23,12 @@ quiet_if_success()
 {
     #local tmp_file=`tmpfile_name quiet_if_success`
     #cleanup_file "$tmp_file"
-    set -x
     local quiet_if_success_quiet=0
     if [ "$1" = "-q" ] ; then
         quiet_if_success_quiet=1
         shift
     fi
-    set -v
+
     local tmp_file="$(bashfoo.mktemp quiet-invocation)"
     if "$@"  > $tmp_file 2>&1 ; then
         return 0
