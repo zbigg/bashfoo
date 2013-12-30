67c6a9c9677b3e519e5c289fee10b382948e7178
z.zagorski <z.zagorski@gmail.com>
2013-12-30 19:10:10 +0000

    queue_read and queue_restore documented

--- queue.sh
+++ queue.sh
@@ -1,7 +1,54 @@
 #
 # queue.sh
 #
+# Filesystem queue routines.
+#
 
+#
+# queue_read
+#
+queue_read()
+    ##   queue_read Q_FOLDER T_FOLDER
+    ##
+    ##   Waits for files to appear in Q_FOLDER and if a file is detected
+    ##   then it atomicatlly moves it to T_FOLDER and yields
+    ##   name of file on stdout as single line.
+    ##
+    ##   All old files are reported on function start.
+    ##   *.tmp files are ignored.
+    ##   inotify is used when available (inotifywait interface is used), 
+    ##   elsewhere, folder is polled periodically (default $QUEUE_READ_SLEEP_INTERVAL=10)
+{
+    if exists_in_path inotifywait ; then
+        queue_read_inotify "$@"
+    else
+        queue_read_sleep "$@"
+    fi
+}
+
+#
+# restore jobs that were not necessarily
+# handled after reading.
+#
+queue_restore()
+    ##   queue_restore Q_FOLDER T_FOLDER
+    ##
+    ##   Puts back all files existing in T_FOLDER back into Q_FOLDER.
+    ##
+    ##   Useful, on start of scripts that use 'queue_read' and were
+    ##   potentially aborted and files shall be processed again.
+{
+    local queue="$1"
+    local target_folder="$2"
+
+    if [ -d "$target_folder" ] ; then
+
+        for file in $(ls -1 "$target_folder") ; do
+            mkdir -p "$queue"
+            mv "$target_folder/$file" "$queue" 
+        done
+    fi
+}
 
 queue_read_inotify()
 {
@@ -62,33 +109,8 @@ queue_read_sleep()
     done
 }
 
-#
-# restore jobs that were not necessarily
-# handled after reading.
-#
-queue_restore()
-{
-    local queue="$1"
-    local target_folder="$2"
-    
-    if [ -d "$target_folder" ] ; then
-        
-        for file in $(ls -1 "$target_folder") ; do
-            mkdir -p "$queue"
-            mv "$target_folder/$file" "$queue" 
-        done
-    fi
-}
-
 exists_in_path()
 {
     type $1 >/dev/null 2>&1
 }
-queue_read()
-{
-    if exists_in_path inotifywait ; then
-        queue_read_inotify "$@"
-    else
-        queue_read_sleep "$@"
-    fi
-}
+
