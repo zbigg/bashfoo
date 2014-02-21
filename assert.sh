9bf54fab2d5ac7d47c42870af7069f65a3d7fceb
z.zagorski <z.zagorski@gmail.com>
2014-02-21 21:43:02 +0000

    assert_... show source code location of assertion

--- assert.sh
+++ assert.sh
@@ -13,15 +13,24 @@
 
 bashfoo_require run
 
+source_log_error()
+{
+    SCRIPT_NAME="${BASH_SOURCE[2]}:${BASH_LINENO[1]}" log_error "$@"
+}
+
+source_log_info()
+{
+    SCRIPT_NAME="${BASH_SOURCE[2]}:${BASH_LINENO[1]}" log_info "$@"
+}
 assert_fails()
 {
     "$@"
     local r=$?
     if [ "$r" = 0 ] ; then
-        log_error "expected '$@' to fail, but it succeeded (exit_code=$r)"
+        source_log_error "expected '$@' to fail, but it succeeded (exit_code=$r)"
         exit 1
     else
-        log_info "success: $@ failed (exit_code=$r) as expected"
+        source_log_info "success: $@ failed (exit_code=$r) as expected"
     fi
 }
 
@@ -30,10 +39,10 @@ assert_succeeds()
     "$@"
     local r=$?
     if [ "$r" != 0 ] ; then
-        log_error "expected '$@' to succeed, but it failed (exit_code=$r)"
+        source_log_error "expected '$@' to succeed, but it failed (exit_code=$r)"
         exit 1
     else
-        log_info "success: $@ succeeded as expected"
+        source_log_info "success: $@ succeeded as expected"
     fi
 }
 
@@ -41,10 +50,10 @@ assert_variable_present()
 {
     local val=`eval echo \\${$1}`
     if [ -z "$val" ] ; then
-        log_error "variable setting '$1' is missing $2"
+        source_log_error "variable setting '$1' is missing $2"
         exit 1
     else
-        log_info "variable $1 value: $val"
+        source_log_info "variable $1 value: $val"
     fi
 }
 
@@ -54,7 +63,7 @@ assert_equals()
     local actual="$2"
     
     if [ "$expected" != "$actual" ] ; then
-        log_error "expected '$expected', but actual value is '$actual'"
+        source_log_error "expected '$expected', but actual value is '$actual'"
         exit 1
     fi
 }
@@ -64,20 +73,20 @@ assert_files_equal()
     local expected="$1"
     local actual="$2"
     if ! quiet_if_success -q diff -u "$expected" "$actual" ; then
-        log_error "file '$actual'(+) is different from '$expected'(-), diff above"
+        source_log_error "file '$actual'(+) is different from '$expected'(-), diff above"
         exit 1
     fi
 }
 
 show_file()
 {
-    log_info "contents of '$1' after last command ($last_command)"
+    source_log_info "contents of '$1' after last command ($last_command)"
     egrep -H "^" $1 >&2
 }
 
 assert_grep() {
     egrep -q "$@" || {
-        log_error "expected '$1' in '$2' not found"
+        source_log_error "expected '$1' in '$2' not found"
         show_file $2
         exit 1
     }
@@ -86,7 +95,7 @@ assert_grep() {
 assert_grepv() {
     
     egrep -qv "$@" || {
-        log_error "not expected '$1' found in $2"
+        source_log_error "not expected '$1' found in $2"
         show_file $2
         exit 1
     }
@@ -96,7 +105,7 @@ assert_exists() {
     local r=0
     for f in "$@" ; do 
         if ! test -f $f ; then
-            log_error "expected file '$f' doesn't exist"
+            source_log_error "expected file '$f' doesn't exist"
             r=1
         fi
     done
